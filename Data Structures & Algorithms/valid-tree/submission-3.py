918a30420d3e7c043e2af9d4d4adb15a8da0bc34
Moises Zuniga <105025319+Moises-ITS@users.noreply.github.com>
2026-09-19 19:51:50 -0400

    Add: valid-tree - submission-3

---
+++ Data Structures & Algorithms/valid-tree/submission-3.py
@@ -0,0 +1,19 @@
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+
+        if n - 1 != len(edges):
+            return False
+        parent = [c for c in range(n)]
+
+        def find(x):
+            if parent[x] != x:
+                parent[x] = find(parent[x])
+            return parent[x]
+        
+        for x, y in edges:
+            rx, ry = find(x), find(y)
+            if rx == ry:
+                return False
+            parent[rx] = ry
+        
+        return True
