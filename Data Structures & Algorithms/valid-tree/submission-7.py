834a6e62edac61eaa11d2e91614ee10608110ac0
Moises Zuniga <105025319+Moises-ITS@users.noreply.github.com>
2026-09-19 20:56:58 -0400

    Add: valid-tree - submission-7

---
+++ Data Structures & Algorithms/valid-tree/submission-7.py
@@ -0,0 +1,24 @@
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        adj = [[] for c in range(n)]
+        inDegree = [0] * n
+        for u, v in edges:
+            adj[u].append(v)
+            adj[v].append(u)
+        
+        visit = set()
+        visit.add(0)
+        q = deque()
+        q.append((0, -1))
+
+        while q:
+            node, parent = q.popleft()
+            for nei in adj[node]:
+                if nei == parent:
+                    continue
+                if nei in visit:
+                    return False
+                visit.add(nei)
+                q.append((nei, node))
+            
+        return len(visit) == n
