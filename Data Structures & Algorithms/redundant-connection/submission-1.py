1ca04dd88ca6c0256defdb97c188791139ed5893
Moises Zuniga <105025319+Moises-ITS@users.noreply.github.com>
2026-09-19 20:54:00 -0400

    Add: redundant-connection - submission-1

---
+++ Data Structures & Algorithms/redundant-connection/submission-1.py
@@ -0,0 +1,28 @@
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+
+        n = len(edges)
+        inDegree = [0] * (n + 1)
+        adj = [[] for c in range(n + 1)]
+
+        for u, v in edges:
+            adj[u].append(v)
+            adj[v].append(u)
+            inDegree[u] += 1
+            inDegree[v] += 1
+        
+        q = deque([c for c in range(1, n + 1) if inDegree[c] == 1])
+
+        while q:
+            curr = q.popleft()
+            inDegree[curr] -= 1
+            for nei in adj[curr]:
+                inDegree[nei] -= 1
+                if inDegree[nei] == 1:
+                    q.append(nei)
+        
+        for u, v in edges[::-1]:
+            if inDegree[v] and inDegree[u] == 2:
+                return [u, v]
+        
+        return []
